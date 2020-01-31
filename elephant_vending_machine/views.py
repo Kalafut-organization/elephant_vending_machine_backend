ac097ce3b48ee2ac9850d49389512e6eef2e650e
Nick Male <nmale98@gmail.com>
2020-01-31 12:10:04 -0500

    Adding initial dummy route for running trials

--- elephant_vending_machine/views.py
+++ elephant_vending_machine/views.py
@@ -7,6 +7,8 @@ a lot of routes.
 # Circular import OK here. See https://flask.palletsprojects.com/en/1.1.x/patterns/packages/
 # pylint: disable=cyclic-import
 from elephant_vending_machine import APP
+from flask import request
+from flask_api import status
 
 @APP.route('/')
 def index():
@@ -20,3 +22,19 @@ def index():
         HTTP response OK with payload 'Hello Elephants'
     """
     return 'Hello Elephants!'
+
+@APP.route('/run-trial', methods=['GET', 'POST'])
+def run_trial():
+    """Responds with 'Running {trial_name}' string
+
+    All requests sent to this route should have a trial_name in
+    the query string, otherwise a 400 error will be returned
+
+    Returns:
+        HTTP response OK or Bad Request with payload 'Running {trial_name}'
+    """
+    if(request.args.get('trial_name') != None):
+        return 'Running ' + request.args.get('trial_name')
+    else:
+        return 'No trial_name specified'
+
--- tests/test_views.py
+++ tests/test_views.py
@@ -13,3 +13,11 @@ def client():
 def test_root_route(client):
     response = client.get('/')
     assert b'Hello Elephants!' in response.data
+
+def test_run_trial_route_sucess(client):
+    response = client.get('/run-trial?trial_name=demo')
+    assert b'Running demo' in response.data
+
+def test_run_trial_route_empty_query_string(client):
+    response = client.get('/run-trial')
+    assert b'No trial_name specified' in response.data
