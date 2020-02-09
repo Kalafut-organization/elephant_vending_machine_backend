afed189c37b713fac2028963339c1fdeb08a03ce
Alex Waibel <alex@waibel.us>
2020-02-08 21:07:05 -0500

    Add experiment logger test

--- tests/test_experiment_logger.py
+++ tests/test_experiment_logger.py
@@ -1,5 +1,6 @@
 import csv
 import io
+import logging
 import pytest
 import re
 
@@ -11,6 +12,16 @@ class MockLogRecord:
 
     def getMessage(self):
         return self.message
+
+class MockFileHandler:
+    def __init__(self, path):
+        self.path = path
+
+    def setLevel(self, log_level):
+        self.log_level = log_level
+
+    def setFormatter(self, formatter):
+        self.formatter = formatter
 
 formatter = CsvFormatter()
 
@@ -44,3 +55,9 @@ def test_csv_formatter_format_message_with_double_quotes():
     LOG_ENTRY_REGEX = r'^\"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}\",\"this text \"\"contains a quote\"\"\"$'
     formatted_message = formatter.format(MockLogRecord(message))
     assert re.match(LOG_ENTRY_REGEX, formatted_message)
+
+def test_create_experiment_logger(monkeypatch):
+    monkeypatch.setattr('elephant_vending_machine.libraries.experiment_logger.create_experiment_logger', lambda path: MockFileHandler(path))
+    exp_logger = create_experiment_logger('unittest.csv')
+    assert exp_logger.level == logging.INFO
+    assert exp_logger.name == 'experiment_logger'
