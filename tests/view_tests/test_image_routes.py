c1006c006556fa53dac1de7772f9f71a41ef6321
Nick Male <nmale98@gmail.com>
2020-02-14 15:43:07 -0500

    Mocking image saving in tests so that testing does not generate any files

--- tests/view_tests/test_image_routes.py
+++ tests/view_tests/test_image_routes.py
@@ -1,13 +1,8 @@
 import pytest
 from io import BytesIO
+from mock import patch
 
 from elephant_vending_machine import elephant_vending_machine
-
-class MockLogger:
-
-    @staticmethod
-    def info(*args, **kwargs):
-        return 
 
 @pytest.fixture
 def client():
@@ -34,8 +29,10 @@ def test_post_image_route_with_file_bad_extension(client):
     assert response.status_code == 400
     assert b'Error with request: File extension not allowed.' in response.data
 
-def test_post_image_route_with_file(client):
+@patch('werkzeug.datastructures.FileStorage.save')
+def test_post_image_route_with_file(mock_save, client):
     data = {'file': (BytesIO(b"Testing: \x00\x01"), 'test_file.png')}
+    mock_save.return_value = ""
     response = client.post('/image', data=data) 
     assert response.status_code == 201
     assert b'Success: Image saved.' in response.data
