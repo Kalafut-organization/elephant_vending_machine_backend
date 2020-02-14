29c647e7323a6e8a89a962f587bf2b3e35bfa0e3
Nick Male <nmale98@gmail.com>
2020-02-14 16:07:16 -0500

    Switching to using monkeypatch, removed mock dependency

--- tests/view_tests/test_image_routes.py
+++ tests/view_tests/test_image_routes.py
@@ -1,6 +1,5 @@
 import pytest
 from io import BytesIO
-from mock import patch
 
 from elephant_vending_machine import elephant_vending_machine
 
@@ -29,10 +28,9 @@ def test_post_image_route_with_file_bad_extension(client):
     assert response.status_code == 400
     assert b'Error with request: File extension not allowed.' in response.data
 
-@patch('werkzeug.datastructures.FileStorage.save')
-def test_post_image_route_with_file(mock_save, client):
+def test_post_image_route_with_file(monkeypatch, client):
+    monkeypatch.setattr('werkzeug.datastructures.FileStorage.save', lambda a, b: "" )
     data = {'file': (BytesIO(b"Testing: \x00\x01"), 'test_file.png')}
-    mock_save.return_value = ""
     response = client.post('/image', data=data) 
     assert response.status_code == 201
     assert b'Success: Image saved.' in response.data
