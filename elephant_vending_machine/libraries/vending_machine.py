10f449070d6b0c4e3d7aa87245363b00f49d7ccb
Nick Male <nmale98@gmail.com>
2020-03-02 00:52:28 -0500

    Partial implementation

--- elephant_vending_machine/libraries/vending_machine.py
+++ elephant_vending_machine/libraries/vending_machine.py
@@ -12,9 +12,9 @@ and associated sensors on the vending machines.
 """
 
 import multiprocessing as mp
-import time
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import MotionSensor, LED
+
 
 LEFT_SCREEN = 9
 MIDDLE_SCREEN = 10
@@ -22,6 +22,8 @@ RIGHT_SCREEN = 11
 MOTION_PIN_VERTICAL = 1
 MOTION_PIN_HORIZONTAL = 2
 LED_PIN = 3
+RED = 33
+GREEN = 34
 
 class VendingMachine:
     """Provides an abstraction of the physical 'vending machine'.
@@ -39,27 +41,27 @@ class VendingMachine:
             addresses (list): A list of local IP addresses of the Raspberry Pis
         """
 
-        self.left_group = self.SensorGrouping(addresses[0], LEFT_SCREEN)
-        self.middle_group = self.SensorGrouping(addresses[1], MIDDLE_SCREEN)
-        self.right_group = self.SensorGrouping(addresses[2], RIGHT_SCREEN)
+        self.left_group = SensorGrouping(addresses[0], LEFT_SCREEN)
+        self.middle_group = SensorGrouping(addresses[1], MIDDLE_SCREEN)
+        self.right_group = SensorGrouping(addresses[2], RIGHT_SCREEN)
 
-    def wait_for_input():
+    def wait_for_input(self):
         """Waits for input on the IR sensors.
 
         Returns:
-            group: A constant corresponding to the group_id of the first sensor tripped
+            group: A constant (int) corresponding to the group_id of the first sensor tripped
         """
         if __name__ == "__main__":
             pool = mp.Pool()
             result = [None] * 3
-            for i in range(LEFT_SCREEN,RIGHT_SCREEN):
+            for i in range(LEFT_SCREEN, RIGHT_SCREEN):
                 pool.apply_async(func=worker, args=(i,), callback=callback)
             pool.close()
             pool.join()
-            final_value = [ r for r in result if r is not None ]
+            final_value = [r for r in result if r is not None]
             return final_value
 
-    def worker(i):
+    def worker(self, i):
         """ The function which is used by the pool. It calls the wait_for_detection() method
             for the passed associated SensorGrouping and returns the value returned by that method.
         """
@@ -73,29 +75,55 @@ class VendingMachine:
             group = self.right_group
         return group.wait_for_detection()
 
-    def callback(t):
-        if t in [ LEFT_SCREEN, MIDDLE_SCREEN, RIGHT_SCREEN ]:
+    def callback(self, t):
+        if t in [LEFT_SCREEN, MIDDLE_SCREEN, RIGHT_SCREEN]:
             pool.terminate()
 
-    class SensorGrouping:
-        """Sensor interactions
+class SensorGrouping:
+    """Sensor interactions
 
-        An abstraction of the sensors associated with a physical
-        screen on the vending machine.
+    An abstraction of the sensors associated with a physical
+    screen on the vending machine.
+    """
+    def __init__(self, address, screen_identifier):
+        self.factory = PiGPIOFactory(host=address)
+        self.group_id = screen_identifier
+        self.ir_sensor_vertical = MotionSensor(MOTION_PIN_VERTICAL, pin_factory=self.factory)
+        self.ir_sensor_horizontal = MotionSensor(MOTION_PIN_HORIZONTAL, pin_factory=self.factory)
+        self.led = LED(LED_PIN, pin_factory=self.factory)
+        self.correct_stimulus = false
+
+    def change_led(self, state):
+        """Controls the lighting of LEDs for a given sensor grouping.
+
+        Parameters:
+            state (int): The desired state of the LED.
         """
-        def __init__(self, address, screen_identifier):
-            self.factory = PiGPIOFactory(host=address)
-            self.group_id = screen_identifier
-            self.ir_sensor_vertical = MotionSensor(MOTION_PIN_VERTICAL, pin_factory=self.factory)
-            self.ir_sensor_horizontal = MotionSensor(MOTION_PIN_HORIZONTAL, pin_factory=self.factory)
-            self.led = LED(LED_PIN, pin_factory=self.factory)
 
-        def light_led():
-            """Controls the lighting of LEDs for a given sensor grouping.
-            """
+    def display_on_screen(self, stimuli_name, correct_answer):
+        """Displays the specified stimuli on the screen.
 
-        def wait_for_detection():
-            """Waits until the motion sensor is activated and returns the group id
-            """
-            self.ir_sensor_vertical.wait_for_motion()
-            return self.group_id
+        Parameters:
+            stimuli_name (str): The name of the file corresponding to the desired
+                                stimuli to be displayed.
+            correct_answer (boolean): Denotes whether this is the desired selection.
+        """
+        self.correct_stimulus = correct_answer
+
+    def wait_for_detection_vertical(self):
+        """Waits until the motion sensor is activated and returns the group id
+
+        Returns:
+            group_id: The group id indicating which SensorGrouping had it's IR sensor(s) triggered first.
+        """
+        self.ir_sensor_vertical.wait_for_motion()
+        return self.group_id
+
+     def wait_for_detection_horizontal(self):
+        """Waits until the motion sensor is activated and returns the group id
+
+        Returns:
+            group_id: The group id indicating which SensorGrouping had it's IR sensor(s) triggered first.
+        """
+        self.ir_sensor_horizontal.wait_for_motion()
+        return self.group_id
--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,7 +1,7 @@
-from elephant_vending_machine.libraries.vending_machine import VendingMachine
+from elephant_vending_machine.libraries.vending_machine import VendingMachine, SensorGrouping
 
 def test_wait_for_input(monkeypatch):
-    monkeypatch.setattr('elephant_vending_machine.libraries.vending_machine.VendingMachine.SensorGrouping.wait_for_detection', lambda: 9)
+    monkeypatch.setattr('elephant_vending_machine.libraries.vending_machine.SensorGrouping.wait_for_detection', lambda: 9)
     vending_machine = VendingMachine(['1','2','3'])
     sensor_grouping = vending_machine.left_group
     assert vending_machine.wait_for_input() == 9
