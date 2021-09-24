1897a09244c981ff4a4515b47235a584dead8432
brogers856 <benrogers86@gmail.com>
2021-09-24 18:42:53 -0400

    Fixed indentation issues in example experiment

--- elephant_vending_machine/static/experiment/example_experiment.py
+++ elephant_vending_machine/static/experiment/example_experiment.py
@@ -14,7 +14,7 @@ def run_experiment(experiment_logger, vending_machine):
     """
 
     NUM_TRIALS = 20
-	INTERTRIAL_INTERVAL = 5 # seconds
+    INTERTRIAL_INTERVAL = 5 # seconds
     BLANK_SCREEN = 'all_black_screen.png'
     FIXATION_STIMULI = 'fixation_stimuli.png'
     WHITE_STIMULI = 'white_stimuli.png'
@@ -25,26 +25,26 @@ def run_experiment(experiment_logger, vending_machine):
         trial_num = trial_index + 1
         experiment_logger.info("Trial %s started", trial_num)
 		
-		vending_machine.left_group.display_on_screen(BLANK_SCREEN)
-		vending_machine.middle_group.display_on_screen(FIXATION_STIMULI)
-		vending_machine.right_group.display_on_screen(BLANK_SCREEN)
-		experiment_logger.info("Presented fixation cross")
+        vending_machine.left_group.display_on_screen(BLANK_SCREEN)
+        vending_machine.middle_group.display_on_screen(FIXATION_STIMULI)
+        vending_machine.right_group.display_on_screen(BLANK_SCREEN)
+        experiment_logger.info("Presented fixation cross")
 		
-		correct_response = False
+        correct_response = False
 		
-		while not correct_response:
+        while not correct_response:
             # Wait for choice on left, middle, or right screens. Timeout if no selection after 5 minutes (300000 milliseconds)
-			selection = vending_machine.wait_for_input([vending_machine.left_group, vending_machine.middle_group, vending_machine.right_group], 30000)
+            selection = vending_machine.wait_for_input([vending_machine.left_group, vending_machine.middle_group, vending_machine.right_group], 30000)
 
-			if selection == 'middle':
-				experiment_logger.info("Trial %s picked middle when selecting fixation cross", trial_num)
+            if selection == 'middle':
+                experiment_logger.info("Trial %s picked middle when selecting fixation cross", trial_num)
                 # Flash middle screen LEDs green for 3 seconds (3000 milliseconds)
                 vending_machine.middle_group.led_color_with_time(0, 255, 0, 3000)
-				correct_response = True
-			elif selection == 'left':
-				experiment_logger.info("Trial %s picked left when selecting fixation cross", trial_num)
-			elif selection == 'right':
-				experiment_logger.info("Trial %s picked right when selecting fixation cross", trial_num)
+                correct_response = True
+            elif selection == 'left':
+                experiment_logger.info("Trial %s picked left when selecting fixation cross", trial_num)
+            elif selection == 'right':
+                experiment_logger.info("Trial %s picked right when selecting fixation cross", trial_num)
             else:
                 experiment_logger.info("Trial %s timed out when waiting to select fixation cross", trial_num)
 
@@ -53,19 +53,19 @@ def run_experiment(experiment_logger, vending_machine):
 
         if white_on_left:
             vending_machine.left_group.display_on_screen(WHITE_STIMULI)
-			vending_machine.middle_group.display_on_screen(FIXATION_STIMULI)
+            vending_machine.middle_group.display_on_screen(FIXATION_STIMULI)
             vending_machine.right_group.display_on_screen(BLACK_STIMULI)
             experiment_logger.info("Trial %s correct stimuli displayed on left", trial_num)
         else:
             vending_machine.left_group.display_on_screen(BLACK_STIMULI)
-			vending_machine.middle_group.display_on_screen(FIXATION_STIMULI)
+            vending_machine.middle_group.display_on_screen(FIXATION_STIMULI)
             vending_machine.right_group.display_on_screen(WHITE_STIMULI)
             experiment_logger.info("Trial %s correct stimuli displayed on right", trial_num)
 
         selection = vending_machine.wait_for_input([vending_machine.left_group, vending_machine.right_group], 300000)
 
-		if selection == 'timeout':
-			experiment_logger.info("Trial %s no selection made.", trial_num)
+        if selection == 'timeout':
+            experiment_logger.info("Trial %s no selection made.", trial_num)
         elif selection == 'left':
             experiment_logger.info("Trial %s picked left", trial_num)
         else:
@@ -73,11 +73,11 @@ def run_experiment(experiment_logger, vending_machine):
 
         experiment_logger.info("Trial %s finished", trial_num)
 		
-		experiment_logger.info("Start of intertrial interval")
+        experiment_logger.info("Start of intertrial interval")
 		
 		# Wait for intertrial interval
-		time.sleep(INTERTRIAL_INTERVAL)
+        time.sleep(INTERTRIAL_INTERVAL)
 		
-		experiment_logger.info("End of intertrial interval")
+        experiment_logger.info("End of intertrial interval")
 
     experiment_logger.info("Experiment finished")
