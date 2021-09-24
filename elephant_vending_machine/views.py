ff0c5356487b8e390c2693e54473e9d365b3bd36
brogers856 <benrogers86@gmail.com>
2021-09-24 17:10:01 -0400

    Fixing linting issues

--- elephant_vending_machine/views.py
+++ elephant_vending_machine/views.py
@@ -184,7 +184,7 @@ def upload_image():
                 add_remote_image(save_path, filename)
             except CalledProcessError:
                 if filename in os.listdir(save_path):
-                  os.remove(os.path.join(save_path, filename))
+                    os.remove(os.path.join(save_path, filename))
                 response = "Error: Failed to copy file to hosts.", \
                   "Image not saved, please try again"
                 response_code = 500
