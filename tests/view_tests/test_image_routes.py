9830d9b4b5abe1e118209134b6a09198e75c811d
brogers856 <benrogers86@gmail.com>
2021-09-26 14:32:40 -0400

    Fixed coverage

--- tests/view_tests/test_image_routes.py
+++ tests/view_tests/test_image_routes.py
@@ -47,6 +47,7 @@ def test_post_image_route_with_file(monkeypatch, client):
 def test_post_image_route_copying_exception(monkeypatch, client):
     monkeypatch.setattr('werkzeug.datastructures.FileStorage.save', lambda save_path, filename: "" )
     monkeypatch.setattr('subprocess.run', lambda command, check, shell: raise_(CalledProcessError(1, ['ssh'])))
+    subprocess.call(["touch", "elephant_vending_machine/static/img/test_file.jpg"])
     data = {'file': (BytesIO(b"Testing: \x00\x01"), 'test_file.png')}
     response = client.post('/image', data=data) 
     assert response.status_code == 500
