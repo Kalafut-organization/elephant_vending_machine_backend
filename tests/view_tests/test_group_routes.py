3bb0433404c4935cc23413a635db6c944f25b409
brogers856 <rogers.1264@buckeyemail.osu.edu>
2021-10-06 20:07:42 -0400

    Fixing tests

--- tests/view_tests/test_group_routes.py
+++ tests/view_tests/test_group_routes.py
@@ -26,29 +26,29 @@ def test_post_group_route_no_name(client):
     assert b'Error with request: No name field in body of request.' in response.data
 
 def test_post_group_route_empty_name(client):
-    data = ''
+    data = {'name': ''}
     response = client.post('/groups', data=data) 
     assert response.status_code == 400
     assert json.loads(response.data)['message'] == 'Error with request: Group name must not be empty.'
 
 def test_post_group_route_duplicate(client):
     subprocess.call(["mkdir", "elephant_vending_machine/static/img/test"])
-    data = 'test'
+    data = {'name': 'test'}
     response = client.post('/groups', data=data)
     assert response.status_code == 400
     assert json.loads(response.data)['message'] == 'Error with request: Group already exists.'
 
 def test_post_group_route_copying_exception(monkeypatch, client):
-    monkeypatch.setattr('subprocess.run', lambda command, check, shell: CompletedProcess(['some_command'], returncode=0))
-    data = 'test'
+    monkeypatch.setattr('subprocess.run', lambda command, check, shell: raise_(CalledProcessError(1, ['ssh'])))
+    data = {'name': 'test'}
     response = client.post('/groups', data=data)
     assert response.status_code == 400
     assert b'Error: Failed to create group on hosts. ", \
         "Group not created, please try again' in response.data
 
 def test_post_group_route_happy_path(monkeypatch, client):
-    monkeypatch.setattr('subprocess.run', lambda command, check, shell: raise_(CalledProcessError(1, ['ssh'])))
-    data = 'test'
+    monkeypatch.setattr('subprocess.run', lambda command, check, shell: CompletedProcess(['some_command'], returncode=0))
+    data = {'name': 'test'}
     response = client.post('/groups', data=data)
     assert response.status_code == 201
     assert b'Success: Group created.'
