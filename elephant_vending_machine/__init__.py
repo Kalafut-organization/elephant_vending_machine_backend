d35116706224f20d9d30b5a8066682065b2162d9
brogers856 <rogers.1264@buckeyemail.osu.edu>
2021-10-22 12:27:15 -0400

    Added ssh commands to run remote button reading

--- elephant_vending_machine/__init__.py
+++ elephant_vending_machine/__init__.py
@@ -6,6 +6,8 @@ resolve to the correct package.
 """
 from flask import Flask
 from flask_cors import CORS, cross_origin
+import subprocess
+import atexit
 
 APP = Flask(__name__)
 CORS(APP)
@@ -14,6 +16,18 @@ APP.config.update(
     REMOTE_HOST_USERNAME='pi',
     REMOTE_IMAGE_DIRECTORY='/home/pi/elephant_vending_machine/images'
 )
+
+for host in APP.config['REMOTE_HOSTS']:
+    user = APP.config['REMOTE_HOST_USERNAME']
+    subprocess.Popen(['ssh', f'''{user}@{host}''', 'python', '/home/pi/button/button.py'])
+
+def kill_processes():
+    for host in APP.config['REMOTE_HOSTS']:
+        user = APP.config['REMOTE_HOST_USERNAME']
+        subprocess.Popen(['ssh', f'''{user}@{host}''', 'pkill', '-f', 'button.py'])
+    print("Test")
+
+atexit.register(kill_processes)
 
 # Circular imports are bad, but views are not used here, only imported, so it's OK
 # pylint: disable=wrong-import-position
