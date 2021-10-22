e0fe659dcff7355f246155657b538dd624cd6858
brogers856 <rogers.1264@buckeyemail.osu.edu>
2021-10-22 13:30:04 -0400

    Fixed linting

--- elephant_vending_machine/__init__.py
+++ elephant_vending_machine/__init__.py
@@ -4,10 +4,10 @@ This module creates the Flask appliaction object so that each
 module can import it safely and the __name__ variable will always
 resolve to the correct package.
 """
-from flask import Flask
-from flask_cors import CORS, cross_origin
 import subprocess
 import atexit
+from flask import Flask
+from flask_cors import CORS, cross_origin
 
 APP = Flask(__name__)
 CORS(APP)
@@ -17,11 +17,12 @@ APP.config.update(
     REMOTE_IMAGE_DIRECTORY='/home/pi/elephant_vending_machine/images'
 )
 
-for host in APP.config['REMOTE_HOSTS']:
-    user = APP.config['REMOTE_HOST_USERNAME']
-    subprocess.Popen(['ssh', f'''{user}@{host}''', 'python', '/home/pi/button/button.py'])
+for remote_host in APP.config['REMOTE_HOSTS']:
+    remote_user = APP.config['REMOTE_HOST_USERNAME']
+    subprocess.Popen(['ssh', f'''{remote_user}@{remote_host}''', 'python', '/home/pi/button/button.py'])
 
 def kill_processes():
+    """Kills remote button reading processes on monitor pis"""
     for host in APP.config['REMOTE_HOSTS']:
         user = APP.config['REMOTE_HOST_USERNAME']
         subprocess.Popen(['ssh', f'''{user}@{host}''', 'pkill', '-f', 'button.py'])
