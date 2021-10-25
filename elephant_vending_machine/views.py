45b8e15476b049818b97c7b3ecf00b36c131323e
Sam Baker <baker.2608@osu.edu>
2021-10-25 15:55:46 -0400

    added check into make experiment from form

--- elephant_vending_machine/views.py
+++ elephant_vending_machine/views.py
@@ -433,13 +433,16 @@ def create_experiment_from_form():
     filedata = filedata.replace("STIMULI_OUTCOMES = []", outcome_trays)
     #save new experiment file in experiments and overwite
     name = request.form['name']
-    filepath = ( \
-        "elephant_vending_machine_backend/elephant_vending_machine/static/experiment/" \
-        + name + ".py")
-    with open(filepath, 'w') as file:
-        file.write(filedata)
-    #Upload experiment
-    file.upload_experiment()
+    if(allowed_experiment(name))
+        filepath = ( \
+            "elephant_vending_machine_backend/elephant_vending_machine/static/experiment/" \
+            + name + ".py")
+        with open(filepath, 'w') as file:
+            file.write(filedata)
+        #Upload experiment
+        file.upload_experiment()
+    else
+         response = "Error with request: File extension not allowed."
     return make_response(jsonify({'message':response}), response_code)
 
 @APP.route('/experiment', methods=['POST'])
